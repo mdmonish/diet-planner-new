49c0e5e9900be4d27d12392f1cbaae056e5f418f
md <mdmonish143@gmail.com>
2024-07-28 14:21:08 +0530

    error fixex with 5 meals initial, end date with respect to start date which wil be disabled, reset is on,y resetting date

--- src/App.js
+++ src/App.js
@@ -1,38 +1,39 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import DayMealContainer from "./components/DayMealContainer";
-import { mealData } from "./data";
+import { mealData, DEFAULT_MEAL_DATA } from "./data";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import PDF from "./PDF";
-import html2canvas from "html2canvas";
-import jsPDF from "jspdf";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { PlusCircle } from "@phosphor-icons/react";
+import { addDays } from "date-fns";
 
-const DEFAULT_WEEK = { startDate: "", list: [] };
+const DEFAULT_WEEK = { startDate: "", endDate: "", list: [] };
 const DEFAULT_MEAL = {
-  mealId: 1,
+  mealId: "",
   mealDetails: {
     mealType: "",
     time: "",
   },
   Days: [
-    { dayId: 1, day: "Day 1", items: [] },
-    { dayId: 2, day: "Day 2", items: [] },
-    { dayId: 3, day: "Day 3", items: [] },
-    { dayId: 4, day: "Day 4", items: [] },
-    { dayId: 5, day: "Day 5", items: [] },
-    { dayId: 6, day: "Day 6", items: [] },
-    { dayId: 7, day: "Day 7", items: [] },
+    { dayId: "", day: "Day 1", items: [] },
+    { dayId: "", day: "Day 2", items: [] },
+    { dayId: "", day: "Day 3", items: [] },
+    { dayId: "", day: "Day 4", items: [] },
+    { dayId: "", day: "Day 5", items: [] },
+    { dayId: "", day: "Day 6", items: [] },
+    { dayId: "", day: "Day 7", items: [] },
   ],
 };
 
 function App() {
   const days = ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7"];
 
-  const [dietDetails, setDietDetails] = useState([]);
+  const [dietDetails, setDietDetails] = useState(DEFAULT_MEAL_DATA);
   const [actions, setActions] = useState("");
   const [selectedDate, setSelectedDate] = useState("");
+  const [endDate, setEndDate] = useState("");
   const [weeklyMeal, setWeeklyMeal] = useState("");
   const [selectedWeek, setSelectedWeek] = useState(DEFAULT_WEEK);
   const [isEnable, setIsEnable] = useState(false);
@@ -41,105 +42,104 @@ function App() {
   //     setDietDetails(dietDetails);
   //   }
   // }, [dietDetails]);
-
-  const generatePDF = () => {
-    const input = document.getElementById("pdf-content");
-
-    html2canvas(input).then((canvas) => {
-      const imgData = canvas.toDataURL("image/png");
-      const pdf = new jsPDF();
-      const imgWidth = 210; // PDF page width
-      const pageHeight = 295; // PDF page height
-      const imgHeight = (canvas.height * imgWidth) / canvas.width;
-      let heightLeft = imgHeight;
-      let position = 0;
-
-      pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
-      heightLeft -= pageHeight;
-
-      while (heightLeft >= 0) {
-        position = heightLeft - imgHeight;
-        pdf.addPage();
-        pdf.addImage(imgData, "PNG", 0, position, imgWidth, imgHeight);
-        heightLeft -= pageHeight;
-      }
-
-      pdf.save("download.pdf");
-    });
-  };
+  useEffect(() => {
+    if (dietDetails) {
+      setSelectedWeek((prev) => ({ ...prev, list: dietDetails }));
+    }
+  }, [dietDetails]);
 
   const handleDate = (val) => {
     setSelectedDate(val);
-    setSelectedWeek((prev) => ({ ...prev, startDate: val }));
+    const futureDate = addDays(val, 6);
+    setEndDate(futureDate);
+
+    setSelectedWeek((prev) => ({
+      ...prev,
+      startDate: val,
+      endDate: futureDate,
+    }));
   };
   const handleAddMeal = () => {
+    console.log("DEFAULT_MEAL", DEFAULT_MEAL);
+    let DEFAULT_MEAL_NEW = { ...DEFAULT_MEAL };
+    DEFAULT_MEAL_NEW = { ...DEFAULT_MEAL_NEW, mealId: dietDetails?.length + 1 };
+    DEFAULT_MEAL_NEW.mealDetails = {
+      ...DEFAULT_MEAL_NEW.mealDetails,
+      mealType: `Meal ${dietDetails?.length + 1}`,
+    };
+    for (var i = 0; i < DEFAULT_MEAL_NEW.Days.length; i++) {
+      DEFAULT_MEAL_NEW.Days[i] = {
+        ...DEFAULT_MEAL_NEW.Days[i],
+        dayId: dietDetails?.length + `${i + 1}`,
+      };
+    }
+
     setIsEnable(true);
     setSelectedWeek((prev) => ({
       ...prev,
-      list: [...prev.list, DEFAULT_MEAL],
+      list: [...prev.list, DEFAULT_MEAL_NEW],
     }));
-    setDietDetails((prev) => [...prev, DEFAULT_MEAL]);
+    setDietDetails((prev) => [...prev, DEFAULT_MEAL_NEW]);
+  };
+
+  const handleReset = () => {
+    console.log(DEFAULT_WEEK, DEFAULT_MEAL_DATA);
+    setSelectedDate("");
+    setEndDate("");
+    setSelectedWeek(DEFAULT_WEEK);
+    setDietDetails(DEFAULT_MEAL_DATA);
   };
 
   return (
     <div className="App">
-      <PDFDownloadLink document={<PDF mealData={mealData} />} fileName="md.pdf">
-        {({ blob, url, loading, error }) =>
-          loading ? "Loading document..." : "Download now!"
-        }
-      </PDFDownloadLink>
-      <div>
-        <div id="pdf-content">
-          {/* Your content goes here */}
-          <h1>Hello, PDF!</h1>
-          <p>This is a sample HTML content that will be converted to PDF.</p>
-        </div>
-
-        <button onClick={generatePDF}>Download PDF</button>
-      </div>
       <div className="flex justify-between pl-4">
-        {" "}
         <DatePicker
           showIcon
           selected={selectedDate}
           dateFormat="dd/MM/yyyy"
           onChange={(date) => handleDate(date)}
           minDate={new Date()}
+          placeholderText="Select a start date"
         />
-        {!isEnable ? (
-          <button onClick={handleAddMeal}>Add Meal Item</button>
-        ) : (
-          <>
-            {" "}
-            <button onClick={() => setIsEnable(!isEnable)}>
-              Reset Meal Item
-            </button>
-            <button>Save</button>
-          </>
-        )}
+        <DatePicker
+          showIcon
+          selected={endDate}
+          dateFormat="dd/MM/yyyy"
+          disabled
+          minDate={new Date()}
+          placeholderText="Select a end date"
+        />
+        <>
+          {" "}
+          <button onClick={handleReset}>Reset Meal Item</button>
+          <button>Save</button>
+        </>
       </div>
-      {isEnable && (
-        <div className="container mx-auto">
-          <div className="grid h-[10vh] grid-cols-8 place-items-center">
-            <div></div>
-            {days?.map((day, index) => (
-              <h3
-                className="rounded-lg border border-gray-500 bg-white px-3 py-1 text-gray-800 shadow-lg"
-                key={`${day}~~${index}`}
-              >
-                {day}
-              </h3>
-            ))}
-          </div>
-        </div>
-      )}
 
-      {isEnable && (
-        <DayMealContainer
-          dietDetails={dietDetails}
-          setDietDetails={setDietDetails}
-        />
-      )}
+      <div className="container mx-auto">
+        <div className="grid h-[10vh] grid-cols-8 place-items-center">
+          <div></div>
+          {days?.map((day, index) => (
+            <h3
+              className="rounded-lg border border-gray-500 bg-white px-3 py-1 text-gray-800 shadow-lg"
+              key={`${day}~~${index}`}
+            >
+              {day}
+            </h3>
+          ))}
+        </div>
+      </div>
+
+      <DayMealContainer
+        dietDetails={dietDetails}
+        setDietDetails={setDietDetails}
+      />
+      <PlusCircle
+        className="cursor-pointer"
+        size={32}
+        color="blue"
+        onClick={handleAddMeal}
+      />
     </div>
   );
 }
